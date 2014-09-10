da1c09d3edbbcf4de85a88da39e0cafada93ce16
Chris Stasiak <me@krzysztof-stasiak.com>
2014-09-10 21:41:08 +0200

    Added memcache client

--- memcache/client.go
+++ memcache/client.go
@@ -1,18 +1,53 @@
 package memcache
 
 import (
-	"log"
+	"github.com/bradfitz/gomemcache/memcache"
+
+	"github.com/feedlabs/feedify/config"
 )
 
 type MemcacheClient struct {
-	host string
-	port string
+	host	string
+	port	string
+	conn	*memcache.Client
 }
 
-func (m MemcacheClient) Connect() {
-	log.Printf("%T connected", m)
+func (m *MemcacheClient) Connect() {
+	m.conn = memcache.New(m.host + ":" + m.port)
+}
+
+func (m *MemcacheClient) Set(key string, value string) {
+	m.conn.Set(&memcache.Item{Key: key, Value: []byte(value)})
+}
+
+func (m *MemcacheClient) Get(key string) (value string, err error) {
+	_item, err := m.conn.Get(key)
+	return string(_item.Value), err
+}
+
+func (m *MemcacheClient) GetMulti(keys []string) map[string]string {
+	_items, _ := m.conn.GetMulti(keys)
+
+	items := make(map[string]string, len(_items))
+
+	for _, key := range(keys)  {
+		items[key] = string(_items[key].Value)
+	}
+
+	return items
+}
+
+func (m *MemcacheClient) Delete(key string) error {
+	return m.conn.Delete(key)
+}
+
+func (m *MemcacheClient) DeleteAll() error {
+	return m.conn.DeleteAll()
 }
 
 func NewMemcacheClient() *MemcacheClient {
-	return &MemcacheClient{}
+	host := config.GetConfigKey("memcache::host")
+	port := config.GetConfigKey("memcache::port")
+
+	return &MemcacheClient{host, port, nil}
 }
